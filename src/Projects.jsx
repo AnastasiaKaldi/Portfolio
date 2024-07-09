934b3617e59ac2c70720d89c3326b5e9955337da
AnastasiaKaldi <kaldianastasia@gmail.com>
2024-07-09 15:33:44 +0100

    Carousel 2.0

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,11 +5,11 @@ import "react-multi-carousel/lib/styles.css";
 import Cosmos from "../src/assets/Cosmos.png";
 import Musa from "../src/assets/Musa.png";
 import Under from "../src/assets/underConstruction.jpg";
+import Icon from "../src/assets/code.png";
 
 function About() {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
@@ -26,51 +26,116 @@ function About() {
       items: 1,
     },
   };
+
   return (
     <section className="Projects">
-      <div className="Projects grid grid-cols-2">
-        <h1 className="MyProjects text-white text-7xl">Projects</h1>
+      <h1
+        className="MyProjects text-white text-7xl"
+        style={{ fontFamily: "Playfair Display" }}
+      >
+        Projects
+      </h1>
+      <div className="Carousel ">
         <Carousel responsive={responsive}>
           <div className="card rounded-sm">
+            <img className="webImage rounded-lg" src={Cosmos} alt="Cosmos" />
+            <h3 className="text-white bold font-bold">Cosmos</h3>
+            <p className="price">
+              Cosmos is an educational website designed to explore and present
+              detailed information about various components of the universe.
+              From galaxies to stars and planets, Cosmos offers a comprehensive
+              journey through the cosmos. The website is built using
+              technologies such as React for interactive user interfaces,
+              React-three-fiber for integrating 3D visualizations seamlessly,
+              and CSS for styling to ensure a visually appealing and
+              user-friendly experience.
+            </p>
+            <div style={{ display: "flex", gap: "1px" }}>
+              <button className="button-with-icon rounded-lg">
+                <a
+                  href="https://github.com/AnastasiaKaldi/Cosmos"
+                  className="button-link"
+                >
+                  <img
+                    src={Icon}
+                    alt="icon"
+                    style={{ width: "30px", height: "30px" }}
+                  />
+                </a>
+              </button>
+              <button className="">
+                {" "}
+                <a
+                  href="https://ourcosmos.netlify.app/"
+                  className="button-link"
+                >
+                  Take a Visit
+                </a>
+              </button>
+            </div>
+          </div>
+          <div className="card rounded-sm">
+            <img className="webImage rounded-lg" src={Musa} alt="Musa" />
+            <h3 className="text-white font-bold">Project Musa</h3>
+            <p className="price">
+              Project Musa is an experiment i created as a universiy project.
+              The aim of the project was to investigate whether or not 10-15
+              year-old children could notice the difference between differently
+              recorded sounds. The project contains two identical games that aim
+              to teach children the musical notes. Their only difference is that
+              in Game A the notes were produced by a real piano, while in Game B
+              the notes were produced by a digital midi keyboard. The project
+              was created using React, Kaboom js, and CSS.
+            </p>
+            <div style={{ display: "flex", gap: "1px" }}>
+              <button className="button-with-icon rounded-lg">
+                <a
+                  href="https://github.com/AnastasiaKaldi/MusicGame"
+                  className="button-link"
+                >
+                  <img
+                    src={Icon}
+                    alt="icon"
+                    style={{ width: "30px", height: "30px" }}
+                  />
+                </a>
+              </button>
+
+              <button className="rounded-lg">
+                {" "}
+                <a
+                  href="https://projectmusa.netlify.app/"
+                  className="button-link"
+                >
+                  Take a Visit
+                </a>
+              </button>
+            </div>
+          </div>
+          <div className="card rounded-sm">
             <img
               className="webImage rounded-lg"
-              src={Cosmos}
-              alt="Cosmos"
-            ></img>
-            <h3 className="text-white">Cosmos</h3>
+              src={Under}
+              alt="Under Construction"
+            />
+            <h3 className="text-white font-bold">Restaurant Blog</h3>
             <p className="price">
-              An educational website showcasing information about components of
-              the universe. Some tools that were used in this project include
-              React, React-three-fiber, and CSS.
-            </p>
-          </div>
-          <div className="card rounded-sm">
-            <img className="webImage rounded-lg" src={Musa} alt="Cosmos"></img>
-            <h3 className="text-white">Cosmos</h3>
-            <p className="price">
-              An educational website showcasing information
-            </p>
-          </div>
-          <div className="card rounded-sm">
-            <img className="webImage rounded-lg" src={Under} alt="Cosmos"></img>
-            <h3 className="text-white">Cosmos</h3>
-            <p className="price">
-              An educational website showcasing information
+              A blog website that showcases and rates different restaurants in
+              Leeds and their cuisines.
             </p>
           </div>
           <div className="card rounded-sm">
             <img
               className="webImage rounded-lg"
-              src={Cosmos}
-              alt="Cosmos"
-            ></img>
-            <h3 className="text-white">Cosmos</h3>
+              src={Under}
+              alt="Under Construction"
+            />
+            <h3 className="text-white font-bold">Something</h3>
             <p className="price">
-              An educational website showcasing information
+              An educational website showcasing information.
             </p>
           </div>
         </Carousel>
-        ;
       </div>
     </section>
   );
